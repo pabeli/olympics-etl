56c92b0a0ed9d58f7837cfbda79770460f6c7138
Paula Lima <limapaulabelen@gmail.com>
2021-12-22 10:14:40 -0300

    adding main

---
+++ main.py
@@ -0,0 +1,18 @@
+from utils.extract_data import extract_data
+from utils.transformation import check_if_no_empty_data
+from utils.load import load_data
+import logging
+
+# Logger initialization
+logging.basicConfig(format='[%(levelname)s]: %(message)s', level=logging.DEBUG)
+
+if __name__ == '__main__':
+    # The extract process
+    logging.info('Starting data extract...')
+    dfs = extract_data()
+
+    logging.info('Starting validation process...')
+    dfs = check_if_no_empty_data(dfs)
+
+    logging.info('Starting load process...')
+    load_data(dfs)

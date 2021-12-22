0d262bb3daccf2fae702aef2cfdc645d881a4800
Paula Lima <limapaulabelen@gmail.com>
2021-12-22 16:23:31 -0300

    updating docs

--- main.py
+++ main.py
@@ -10,9 +10,9 @@ if __name__ == '__main__':
     # The extract process
     logging.info('Starting data extract...')
     dfs = extract_data()
-
+    # Transformation process
     logging.info('Starting validation process...')
     dfs = check_if_no_empty_data(dfs)
-
+    # Loading into database
     logging.info('Starting load process...')
     load_data(dfs)
